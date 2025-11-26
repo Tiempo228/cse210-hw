c5b5c5ac704e306d00f9c60c1eb6fecd261ddd05
tiempos <ksanwogou@byupathway.edu>
2025-11-26 13:08:33 +0000

    week05 homework assignment

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+
+public class MathAssignment : Assignment
+{
+    private string _textBookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textBookSection, string problems) : base(studentName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+
+    }
+
+
+    public string GetHomeWorkList()
+    {
+
+        return $"Section: {_textBookSection}\nProblem: {_problems}\n";
+
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        MathAssignment matAassignment = new MathAssignment("Beau Benneth", "Multiplication", "3.7", "14 x 9");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The cause of world war II");
+
+        Console.WriteLine(matAassignment.GetSummary());
+        Console.WriteLine(matAassignment.GetHomeWorkList());
+
+        Console.WriteLine();
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
